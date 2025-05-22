9b805beeef300c1077027536553b4b232ea659ea
chisanucha-plum <chisanucha.plum@gmail.com>
2025-05-22 09:29:22 +0700

    fix code

--- App.java
+++ App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        // เปลี่ยนชื่อคลาสให้ตรงกับที่ใช้งานจริง (Poker)
-        new Poker(); // ไม่ต้องเรียก startGame() ซ้ำ เพราะ Poker() เรียกใน constructor อยู่แล้ว
+        new Poker(); 
     }
 }
